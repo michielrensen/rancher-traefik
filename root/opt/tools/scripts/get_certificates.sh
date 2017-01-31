8f50ab6c5a278a7897ac6ce09f282253ee68c004
Michiel Rensen <michiel.rensen@lightspeedhq.com>
2017-01-31 10:26:24 +0100

    Remove certsfile first

--- root/opt/tools/scripts/get_certificates.sh
+++ root/opt/tools/scripts/get_certificates.sh
@@ -2,9 +2,12 @@
 
 JQ=${JQ:-/opt/tools/scripts/jq}
 CERTPATH=${CERTPATH:-/opt/traefik/certs}
+CERTFILE="${CERTPATH}/certs.cert.pem"
 
 IFS=$'\n' CERTS=($(curl -u "${CATTLE_ACCESS_KEY}:${CATTLE_SECRET_KEY}") "${CATTLE_URL}"/projects/"${PROJECT_ID}/certificates" | $JQ -r '.data[] | .cert' ))
 
+rm $CERTFILE
+
 for ((i = 0; i < "${#CERTS[@]}"; ++i)); do
-  echo "${CERTS[$i]}" >> "${CERTPATH}"/certs.cert.pem
+  echo "${CERTS[$i]}" >> "${CERTFILE}"
 done
