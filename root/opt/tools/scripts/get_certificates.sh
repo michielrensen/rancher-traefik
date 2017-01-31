3105a1cf3cb3251481a5fb9c6c12f52c4feae755
Michiel Rensen <michiel.rensen@lightspeedhq.com>
2017-01-31 11:19:53 +0100

    Fixed syntax error

--- root/opt/tools/scripts/get_certificates.sh
+++ root/opt/tools/scripts/get_certificates.sh
@@ -4,7 +4,7 @@ JQ=${JQ:-/opt/tools/scripts/jq}
 CERTPATH=${CERTPATH:-/opt/traefik/certs}
 CERTFILE="${CERTPATH}/certs.cert.pem"
 
-IFS=$'\n' CERTS=($(curl -u "${CATTLE_ACCESS_KEY}:${CATTLE_SECRET_KEY}") "${CATTLE_URL}"/projects/"${PROJECT_ID}/certificates" | $JQ -r '.data[] | .cert' ))
+IFS=$'\n' CERTS=($(curl -s -u "${CATTLE_ACCESS_KEY}:${CATTLE_SECRET_KEY}" "${CATTLE_URL}"/projects/"${PROJECT_ID}/certificates" | $JQ -r '.data[] | .cert' ))
 
 rm $CERTFILE
 
